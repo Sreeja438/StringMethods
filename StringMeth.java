046a082593a412e481fed482be7da5c93e87e001
Sreeja438 <125867693+Sreeja438@users.noreply.github.com>
2023-02-28 21:56:52 +0530

    Add files via upload

---
+++ StringMeth.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+public class StringMeth {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNextInt()){
+            if(scanner.hasNextInt()){
+                int r=scanner.nextInt();
+                System.out.println("The number is"  + r);
+
+            }else{
+                break;
+            }
+        }
+    }
+    
+}
